94bc92b00552fdb63eecfd6a936afe53144cfb78
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-29 21:13:37 +0000

    missed one - MVP now achieved

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -66,17 +66,22 @@ router.post('', (req, res) => {
   })
 })
 
-// old version
-// router.post('/', (req, res) => {
-//   const title = req.body.title;
-//   const contents = req.body.contents;
-//   res.status(200).json({
-//     post: `title: '${title}', contents: '${contents}'`
-//   });
-//   .catch(error => {
-//     console.log(error);
-//   })
-// });
+router.post('/:id/comments', (req, res) => {
+  const comment = req.body
+  // console.log(req);
+  db.insertComment(req.body)
+  .then(data => {
+    console.log(data);
+    res.status(201).json(comment);
+    res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+  })
+  .catch(error => {
+    //log error to database
+    console.log(error);
+    res.status(400).json({errorMessage: "Please provide text for the comment."})
+    res.status(500).json({error: "There was an error while saving the comment to the database" })
+  })
+})
 
 // Delete requests
 router.delete('/:id', (req, res) => {
