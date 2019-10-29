13af7ca28de6d7f362cf451f76d1b895e0b6dd42
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-29 15:08:45 +0000

    finalised the .post request

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -1,18 +1,38 @@
 const express = require('express');
+const db = require('../data/db');
 
 const router = express.Router();
-
 
 // GET requests
 
 
 // POST requests
-router.post('/', (req, res) => {
-  const title = req.body.title;
-  const contents = req.body.contents;
-  res.status(200).json({
-    post: `title: '${title}', contents: '${contents}'`
-  });
-});
+
+router.post('', (req, res) => {
+  // console.log(req);
+  db.insert(req.body)
+  .then(data => {
+    console.log(data);
+    res.status(201).json(data);
+  })
+  .catch(error => {
+    //log error to database
+    console.log(error);
+    res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    res.status(500).json({error: "There was an error while saving the post to the database" })
+  })
+})
+
+// old version
+// router.post('/', (req, res) => {
+//   const title = req.body.title;
+//   const contents = req.body.contents;
+//   res.status(200).json({
+//     post: `title: '${title}', contents: '${contents}'`
+//   });
+//   .catch(error => {
+//     console.log(error);
+//   })
+// });
 
 module.exports = router;
